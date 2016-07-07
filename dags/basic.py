6aa2ff09ba27648b198432e41cba301b8ad635a3
nwebz <nweber522@gmail.com>
2016-07-07 17:01:11 -0400

    init

---
+++ dags/basic.py
@@ -0,0 +1,44 @@
+from airflow import DAG
+from airflow.operators import BashOperator
+from airflow.operators import EmailOperator
+from datetime import datetime, timedelta
+
+seven_days_ago = datetime.combine(datetime.today() - timedelta(7),
+                                  datetime.min.time())
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': seven_days_ago,
+    'email': ['airflow@airflow.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'schedule_interval': timedelta(1),
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG('basic', default_args=default_args)
+
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+t1 = BashOperator(
+    task_id='list_processes',
+    bash_command='ps aux > running.txt',
+    dag=dag)
+
+
+#dag.doc_md = __doc__
+
+t2 = BashOperator(
+    task_id='view',
+    depends_on_past=False,
+    bash_command='cat running.txt',
+    dag=dag)
+
+
+
+t2.set_upstream(t1)
---
+++ dags/clean_opa3.py
@@ -0,0 +1,66 @@
+""" this is a docstring """
+from airflow import DAG
+from airflow.operators import BashOperator
+from airflow.operators import PythonOperator
+from airflow.operators import EmailOperator
+from datetime import datetime, timedelta
+import tempfile
+
+seven_days_ago = datetime.combine(datetime.today() - timedelta(7),
+                                  datetime.min.time())
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': seven_days_ago,
+    'email': ['airflow@airflow.com'],
+    'email_on_failure': True, 'email_on_retry': False, 'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'schedule_interval': timedelta(1),
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+
+dag = DAG('clean_opa3', default_args=default_args)
+
+def mkdir():
+	return tempfile.mkdtemp()
+
+
+t1 = PythonOperator(
+    task_id='temp_storage',
+    python_callable=mkdir,
+    dag=dag)
+
+
+t2 = BashOperator(
+      task_id='clean_properties',
+      bash_command='''cat ~/opa_data/br63trf.os13sd | phl-properties > {{ ti.xcom_pull("temp_storage") }}/properties.csv''', dag=dag)
+
+
+t3 = BashOperator(
+        task_id='clean_buildings',
+        bash_command='''cat ~/opa_data/br63trf.buildcod | phl-building-codes > {{ ti.xcom_pull("temp_storage") }}/building_codes.csv''', dag=dag)
+
+t4 = BashOperator(
+        task_id='clean_street_codes',
+        bash_command='''cat ~/opa_data/br63trf.stcode | phl-street-codes > {{ ti.xcom_pull("temp_storage") }}/street_codes.csv''', dag=dag)
+
+t5 = BashOperator(
+        task_id='clean_off_property',
+        bash_command='''cat ~/opa_data/br63trf.offpr | phl-off-property > {{ ti.xcom_pull("temp_storage") }}/off_property.csv''', dag=dag)
+
+t6 = BashOperator(
+        task_id='clean_assessment_history',
+        bash_command='''cat ~/opa_data/br63trf.nicrt4wb | phl-assessment-history > {{ ti.xcom_pull("temp_storage") }}/assessment_history.csv''', dag=dag)
+
+
+dag.doc_md = "hello"
+t2.set_upstream(t1)
+t3.set_upstream(t1)
+t4.set_upstream(t1)
+t5.set_upstream(t1)
+t6.set_upstream(t1)
