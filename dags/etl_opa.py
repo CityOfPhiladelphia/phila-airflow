1a78a70f9b3c92e1536e19a73dc630bc4bdbd97a
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2016-08-05 11:59:48 -0400

    Add semi-complete opa workflow dag

--- dags/etl_opa.py
+++ dags/etl_opa.py
@@ -9,10 +9,9 @@ Connections:
 from airflow import DAG
 from airflow.operators import BashOperator
 from airflow.operators import PythonOperator
+from airflow.operators import DatumCSV2TableOperator
 from airflow.operators import FileTransferOperator
-from airflow.operators import FileTransformOperator
 from datetime import datetime, timedelta
-import tempfile
 
 # ============================================================
 # Defaults - these arguments apply to all operators
@@ -27,43 +26,173 @@ default_args = {
     # 'pool': 'backfill',  # TODO: Lookup what pool is
 }
 
-# ------------------------------------------------------------
-# Extract - copy files to the staging S3 bucket
+pipeline = DAG('etl_opa', default_args=default_args)  # TODO: Look up how to schedule a DAG
 
-t0 = FileTransferOperator(
-    task_id='download_property_input',
+# ------------------------------------------------------------
+# Extract - copy files to the staging area
+
+def mkdir():
+    import tempfile
+    return tempfile.mkdtemp()
+
+t0 = PythonOperator(
+    task_id='staging',
+    dag=pipeline,
+
+    python_callable=mkdir,
+)
+
+t1a = FileTransferOperator(
+    task_id='download_properties',
+    dag=pipeline,
 
     source_type='local',
     source_path='/home/mjumbewu/Programming/phila/property-assessments-data-pipeline/input/\'br63trf.os13sd\'',
 
-    dest_type='s3',
-    dest_conn_id='phl-s3-etl',
-    dest_path='s3://airflow-staging/opa/{{ ts_nodash }}/input/br63trf.os13sd',
+    dest_type='local',
+    dest_path='{{ ti.xcom_pull("staging") }}/br63trf.os13sd',
+)
 
-    **default_args,
+t1b = FileTransferOperator(
+    task_id='download_building_codes',
+    dag=pipeline,
+
+    source_type='local',
+    source_path='/home/mjumbewu/Programming/phila/property-assessments-data-pipeline/input/\'br63trf.buildcod\'',
+
+    dest_type='local',
+    dest_path='{{ ti.xcom_pull("staging") }}/br63trf.buildcod',
+)
+
+t1c = FileTransferOperator(
+    task_id='download_street_codes',
+    dag=pipeline,
+
+    source_type='local',
+    source_path='/home/mjumbewu/Programming/phila/property-assessments-data-pipeline/input/\'br63trf.stcode\'',
+
+    dest_type='local',
+    dest_path='{{ ti.xcom_pull("staging") }}/br63trf.stcode',
+)
+
+t1d = FileTransferOperator(
+    task_id='download_off_property',
+    dag=pipeline,
+
+    source_type='local',
+    source_path='/home/mjumbewu/Programming/phila/property-assessments-data-pipeline/input/\'br63trf.offpr\'',
+
+    dest_type='local',
+    dest_path='{{ ti.xcom_pull("staging") }}/br63trf.offpr',
+)
+
+t1e = FileTransferOperator(
+    task_id='download_assessment_history',
+    dag=pipeline,
+
+    source_type='local',
+    source_path='/home/mjumbewu/Programming/phila/property-assessments-data-pipeline/input/\'br63trf.nicrt4wb\'',
+
+    dest_type='local',
+    dest_path='{{ ti.xcom_pull("staging") }}/br63trf.nicrt4wb',
 )
 
 # ------------------------------------------------------------
-# Transform - generate tables suitable for loading into DB
+# Transform - run each table through a cleanup script
 
-t1 = FileTransformOperator(
-    task_id='transform_property_input',
+t2a = BashOperator(
+    task_id='clean_properties',
+    dag=pipeline,
 
-    source_type='s3',
-    source_conn_id='phl-s3-etl',
-    source_path='s3://airflow-staging/opa/{{ ts_nodash }}/input/br63trf.os13sd',
-
-    dest_type='s3',
-    dest_conn_id='phl-s3-etl',
-    dest_path='s3://airflow-staging/opa/{{ ts_nodash }}/output/properties.csv',
-
-    transform_script='phl-properties',
-
-    **default_args,
+    bash_command=
+        'cat {{ ti.xcom_pull("staging") }}/br63trf.os13sd | '
+        'phl-properties > {{ ti.xcom_pull("staging") }}/properties.csv',
 )
+
+t2b = BashOperator(
+    task_id='clean_building_codes',
+    dag=pipeline,
+
+    bash_command=
+        'cat {{ ti.xcom_pull("staging") }}/br63trf.buildcod | '
+        'phl-building-codes > {{ ti.xcom_pull("staging") }}/building_codes.csv',
+)
+
+t2c = BashOperator(
+    task_id='clean_street_codes',
+    dag=pipeline,
+
+    bash_command=
+        'cat {{ ti.xcom_pull("staging") }}/br63trf.buildcod | '
+        'phl-street-codes > {{ ti.xcom_pull("staging") }}/building_codes.csv',
+)
+
+t2d = BashOperator(
+    task_id='clean_off_property',
+    dag=pipeline,
+
+    bash_command=
+        'cat {{ ti.xcom_pull("staging") }}/br63trf.offpr | '
+        'phl-off-property > {{ ti.xcom_pull("staging") }}/off_property.csv',
+)
+
+t2e = BashOperator(
+    task_id='clean_assessment_history',
+    dag=pipeline,
+
+    bash_command=
+        'cat {{ ti.xcom_pull("staging") }}/br63trf.nicrt4wb | '
+        'phl-assessment-history > {{ ti.xcom_pull("staging") }}/assessment_history.csv',
+)
+
 
 # ------------------------------------------------------------
 # Load - copy tables into on-prem database(s)
+
+t3a = DatumCSV2TableOperator(
+    task_id='load_properties',
+    dag=pipeline,
+
+    csv_path='{{ ti.xcom_pull("staging") }}/properties.csv',
+    db_conn_id='phl-warehouse-staging',
+    db_table_name='opa_properties',
+)
+
+t3b = DatumCSV2TableOperator(
+    task_id='load_building_codes',
+    dag=pipeline,
+
+    csv_path='{{ ti.xcom_pull("staging") }}/building_codes.csv',
+    db_conn_id='phl-warehouse-staging',
+    db_table_name='opa_building_codes',
+)
+
+t3c = DatumCSV2TableOperator(
+    task_id='load_street_codes',
+    dag=pipeline,
+
+    csv_path='{{ ti.xcom_pull("staging") }}/street_codes.csv',
+    db_conn_id='phl-warehouse-staging',
+    db_table_name='opa_street_codes',
+)
+
+t3d = DatumCSV2TableOperator(
+    task_id='load_off_property',
+    dag=pipeline,
+
+    csv_path='{{ ti.xcom_pull("staging") }}/off_property.csv',
+    db_conn_id='phl-warehouse-staging',
+    db_table_name='opa_off_property',
+)
+
+t3e = DatumCSV2TableOperator(
+    task_id='load_assessment_history',
+    dag=pipeline,
+
+    csv_path='{{ ti.xcom_pull("staging") }}/assessment_history.csv',
+    db_conn_id='phl-warehouse-staging',
+    db_table_name='opa_assessment_history',
+)
 
 # ------------------------------------------------------------
 # Postscript - clean up the staging area
@@ -79,6 +208,8 @@ t1 = FileTransformOperator(
 # ============================================================
 # Configure the pipeline's dag
 
-pipeline = DAG('etl_opa', default_args=default_args)  # TODO: Look up how to schedule a DAG
-
-pipeline >> t0 >> t1
+t0 >> t1a >> t2a >> t3a
+t0 >> t1b >> t2b >> t3b
+t0 >> t1c >> t2c >> t3c
+t0 >> t1d >> t2d >> t3d
+t0 >> t1e >> t2e >> t3e
--- plugins/datum_plugin.py
+++ plugins/datum_plugin.py
@@ -5,11 +5,25 @@ import datum
 from airflow.plugins_manager import AirflowPlugin
 
 from airflow.exceptions import AirflowException
+from airflow.hooks import BaseHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 
-class CSV2DBOperator(BaseOperator):
+class DatumHook (BaseHook):
+    def __init__(self, db_conn_id, *args, **kwargs):
+        super(DatumHook, self).__init__(*args, **kwargs)
+        self.db_conn_id = db_conn_id
+
+    def get_conn(self):
+        if self.conn is None:
+            params = self.get_connection(self.db_conn_id)
+            logging.info('Establishing connection to {}'.format(params.name))
+            self.conn = datum.connect(params.extra)
+        return self.conn
+
+
+class DatumCSV2TableOperator(BaseOperator):
     """
     Load a CSV file into a database table
 
@@ -19,33 +33,77 @@ class CSV2DBOperator(BaseOperator):
 
     :param db_conn_id: The connection to run the operator against.
     :type db_conn_id: string
+    :param db_table_name: The name of the table to load into.
+    :type db_table_name: string
     :param csv_path: The path to the file on the server
     :type csv_path: string
+    :param truncate: Whether the table should be truncated before inserting.
+    :type truncate: bool
     """
 
-    template_fields = ('remote_path','local_path',)
-    template_ext = ()
-    ui_color = '#f4a460'
+    template_fields = ('db_table_name','csv_path',)
 
     @apply_defaults
     def __init__(self,
-                 conn_id,
-                 remote_path,
-                 local_path,
-                 secure=False, *args, **kwargs):
-        super(CSV2DBOperator, self).__init__(*args, **kwargs)
-        self.conn_id = conn_id
-        self.remote_path = remote_path
-        self.local_path = local_path
-        self.secure = secure
+                 db_conn_id,
+                 db_table_name,
+                 csv_path,
+                 truncate=True,
+                 db_field_overrides=None,
+                 sql_override=None,
+                 *args, **kwargs):
+        super(DatumCSV2TableOperator, self).__init__(*args, **kwargs)
+        self.db_conn_id = db_conn_id
+        self.db_table_name = db_table_name
+        self.csv_path = csv_path
+        self.truncate = truncate
+        self.db_field_overrides = db_field_overrides or {}
+        self.sql_override = sql_override
 
     def execute(self, context):
-        Hook = (FTPSHook if self.secure else FTPHook)
-        conn = Hook(ftp_conn_id=self.conn_id)
-        logging.info("Retrieving file from FTP server")
-        conn.retrieve_file(self.remote_path, self.local_path)
+        logging.info("Connecting to the database")
+        self.hook = DatumHook(db_conn_id=self.db_conn_id)
+        self.conn = self.hook.get_conn()
+
+        table = self.db_table_name
+
+        with open(self.csv_path) as csvfile:
+            rows = csv.DictReader(csvfile)
+
+            self.create_table_if_not_exist(rows.fieldnames)
+            self.truncate_table()
+
+            logging.info("Inserting rows into table, 1000 at a time")
+            self.conn.bulk_insert(table, rows, chunk_size=1000)
+
+        logging.info("Done!")
+
+    def create_table_if_not_exist(self, fieldnames):
+        table = self.db_table_name
+
+        logging.info('Checking whether table {} exists'.format(table))
+        if table.upper() in self.conn.tables:
+            logging.info('Table {} already exists.'.format(table))
+            return
+
+        fielddefs = ',\n'.join(
+            fieldname + ' ' + self.db_field_overrides.get(fieldname, 'VARCHAR2(4000)')
+            for fieldname in fieldnames
+        )
+        sql = self.sql_override or 'CREATE TABLE {} ({})'.format(table, fielddefs)
+
+        logging.info('Creating the table: {}'.format(sql))
+        self.conn.execute(sql)
+
+    def truncate_table(self):
+        table = self.db_table_name
+
+        if self.truncate:
+            logging.info("Truncating the {} table".format(table))
+            self.conn.truncate(table)
 
 
 class DatumPlugin(AirflowPlugin):
     name = "datum_plugin"
-    operators = [CSV2DBOperator]
+    operators = [DatumCSV2TableOperator]
+    hooks = [DatumHook]
