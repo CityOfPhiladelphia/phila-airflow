d7fd34cc7cc63b2556b63e81f812ad1ce0fa60e5
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2016-11-09 05:13:43 -0500

    Update requirements for Python3

--- install.sh
+++ install.sh
@@ -5,10 +5,7 @@ apt-get update
 
 # install phila-airflow dependencies
 apt-get install -y build-essential libssl-dev libffi-dev
-apt-get install -y python python-pip python-setuptools python-dev python-psycopg2 postgresql-client postgresql-client-common 
-
-# install airflow
-pip install "airflow[hive]" cryptography Celery
+apt-get install -y python3 python3-pip python3-setuptools python3-dev postgresql-client postgresql-client-common
 
 # install redis tools
 apt-get install -y redis-tools
@@ -20,12 +17,12 @@ apt-get install -y git alien wget libaio1
 # grab instant sql-plus instant oracle client/ rename downloaded file and install with alien
 wget https://www.dropbox.com/s/ubgeht3m59bhfh1/oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.rpm?dl=0
 mv oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.rpm\?dl\=0 oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.rpm
-alien -i oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.rpm 
+alien -i oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.rpm
 
 # grab instant basic-lite instant oracle client/ rename downloaded file and install with alien
 wget https://www.dropbox.com/s/1yzl0fdnaiw5yqp/oracle-instantclient12.1-basiclite-12.1.0.2.0-1.x86_64.rpm?dl=0
 mv oracle-instantclient12.1-basiclite-12.1.0.2.0-1.x86_64.rpm?dl=0 oracle-instantclient12.1-basiclite-12.1.0.2.0-1.x86_64.rpm
-alien -i oracle-instantclient12.1-basiclite-12.1.0.2.0-1.x86_64.rpm 
+alien -i oracle-instantclient12.1-basiclite-12.1.0.2.0-1.x86_64.rpm
 
 # grab instant oracle-sdk / rename downloaded files and install with alien
 wget https://www.dropbox.com/s/uic5vzc9yobttct/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm?dl=0
@@ -41,13 +38,7 @@ apt-get install -y libgdal-dev libgeos-dev binutils libproj-dev gdal-bin
 apt-get install -y libspatialindex-dev
 
 # install airflow dependencies
-pip install -r ./requirements.txt
-
-# setup airflow environment variables
-export AIRFLOW_HOME=~/phila-airflow
-export AIRFLOW__CORE__EXECUTOR=CeleryExecutor
-export AIRFLOW__CORE__LOAD_EXAMPLES=False
-export AIRFLOW__CORE__SQL_ALCHEMY_CONN=my_conn_string
+pip3 install -r ./requirements.txt
 
 echo 'FINISHED'
 
