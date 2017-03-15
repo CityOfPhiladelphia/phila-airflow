f9da176ca907199aed550c39fe0a2ff81f8ce0c2
Andrew Madonna <andrew.w.madonna@gmail.com>
2017-03-15 12:52:20 -0400

    Cleanup entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,7 +5,6 @@ alias pip=pip3
 
 AIRFLOW_HOME="/usr/local/airflow"
 CMD="airflow"
-: ${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print FERNET_KEY")}
 
 # Install custom python package if requirements.txt is present
 if [ -e "/requirements.txt" ]; then
@@ -21,25 +20,10 @@ else
            eastern_state exports "$EASTERN_STATE_ENV")
 fi
 
-# Generate Fernet key
-sed -i "s|\$FERNET_KEY|$FERNET_KEY|" "$AIRFLOW_HOME"/airflow.cfg
-
 if [ "$1" = "webserver" ]; then
   echo "Initialize database..."
   $CMD initdb
-fi
-
-if [ "x$EXECUTOR" = "xLocal" ]
-then
-  sed -i "s/executor = CeleryExecutor/executor = LocalExecutor/" "$AIRFLOW_HOME"/airflow.cfg
-  exec $CMD "$@"
-else
-  if [ "$1" = "version" ]; then
-    exec $CMD version
-  fi
-  sed -i "s/executor = CeleryExecutor/executor = SequentialExecutor/" "$AIRFLOW_HOME"/airflow.cfg
-  sed -i "s#sql_alchemy_conn = postgresql+psycopg2://airflow:airflow@postgres/airflow#sql_alchemy_conn = sqlite:////usr/local/airflow/airflow.db#" "$AIRFLOW_HOME"/airflow.cfg
-  echo "Initialize database..."
-  $CMD initdb
   exec $CMD webserver
+else
+  exec $CMD "$@"
 fi
